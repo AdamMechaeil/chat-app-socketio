087566a2834d50d988e6d6d34dd0cdf141ae7b0b
Adam Mechaeil <mechaeil66@yahoo.com>
2024-08-20 18:15:59 +0530

    minor bug fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,11 @@
 const express=require("express");
-const { signin, signup, checkLogin } = require("../controllers/auth");
+const { signin, signup, checkLogin, updatePassword } = require("../controllers/auth");
 const authRouter=express.Router();
 
 authRouter.post("/signin",signin);
 authRouter.post("/signup",signup);
 authRouter.post("/checkLogin",checkLogin);
-// authRouter.put("/forgotPassword/:id",updatePassword);
+authRouter.put("/forgotPassword/:id",updatePassword);
 // authRouter.post("/findUserByEmail",findUserByEmail);
 // authRouter.post("/generateOtp",generateOtp);
 // authRouter.post("/verifyOtp",verifyOtp);
