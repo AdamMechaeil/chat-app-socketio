f0ef3bfa9ae8f2e44b8b5bae97fd1eb71918f582
Adam Mechaeil <mechaeil66@yahoo.com>
2024-08-27 18:03:22 +0530

    auth completed

--- client/src/app/Chats/page.js
+++ client/src/app/Chats/page.js
@@ -1,10 +1,8 @@
-"use client"
 import React from 'react'
-import Main from '../Components/Auth/Main'
 
 const page = () => {
   return (
-    <div><Main/></div>
+    <div>page</div>
   )
 }
 
---
+++ client/src/app/Context/AuthContext.js
@@ -0,0 +1,61 @@
+"use client"
+import { createContext, useReducer } from "react";
+import { axios } from "../../utils/Util";
+const initialState = {
+  userId: "",
+  token: "",
+};
+
+async function signin(body) {
+  try {
+    const response = await axios.post(
+      "http://localhost:8000/auth/signin",
+      body
+    );
+    return response?.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function signup(body) {
+  try {
+    const response = await axios.post(
+      "http://localhost:8000/auth/signup",
+      body
+    );
+    return response?.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function reducer(state, action) {
+  try {
+    switch (action.type) {
+      case "SIGN_UP":
+        const signupState={...action.payload};
+        localStorage.setItem("authData5000Weekdays",JSON.stringify(signupState))
+        return signupState;
+        case "SIGN_IN":
+        const signinState={...action.payload};
+        localStorage.setItem("authData5000Weekdays",JSON.stringify(signinState))
+        return signinState;
+      default:
+        return state;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AuthContext.Provider value={{ AuthData: state, dispatch, signin, signup }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
